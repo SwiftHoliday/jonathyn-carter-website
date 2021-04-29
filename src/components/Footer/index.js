c14c6b2bb4d439ad09b7b40be661d8eeb6f94108
Jonathyn Carter <jonathyncarter@gmail.com>
2021-04-28 21:35:37 -0400

    adds sass styles to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
+import * as styles from './Footer.module.scss';
+
 const Footer = (props) => (
-    <footer>
+    <footer className={styles.Footer}>
         <p>
             Copyright &copy; All Rights Reserved
              {' '}{new Date().getFullYear()} Jonathyn Carter
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -3,6 +3,8 @@ import React from 'react';
 import Header from '../Header';
 import Footer from '../Footer';
 
+import '../../styles/reset.scss';
+
 const Layout = (props) => (
     <div>
         <Header />
